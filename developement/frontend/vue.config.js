ca1605193b21bf77b1d13abcf8366ec383eecc65
Peter Jussel <jujussel@gmail.com>
2021-05-21 17:41:09 +0900

    sync

--- developement/frontend/vue.config.js
+++ developement/frontend/vue.config.js
@@ -1,10 +1,20 @@
 const fs = require('fs')
 
+const path = require('path')
+
 module.exports = {
   devServer: {
     https: {
       key: fs.readFileSync('/etc/pki/tls/certs/key.pem'),
       cert: fs.readFileSync('/etc/pki/tls/certs/cert.pem')
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      symlinks: false,
+      alias: {
+        vue: path.resolve('./node_modules/vue')
+      }
     }
   }
 }
