5c88beb65742a23648907f664468ebb65da7b202
jujussel <jujussel@gmail.com>
2021-06-08 07:40:59 +0900

    sync

--- developement/frontend/src/components/cc_patient/cc_patient_new.vue
+++ developement/frontend/src/components/cc_patient/cc_patient_new.vue
@@ -1,17 +1,45 @@
 <template>
     <div class="cc-patient-new-main">
+        <cui-card class="cc-patient-new-header">
+            <template #header>
+                <h2> {{ $lang.patientNew }} </h2>
+                <cui-button plain :label="$lang.cancel" />
+                <cui-button primary :label="$lang.register" />
+            </template>
+        </cui-card>
         <cui-card>
             <template #header>
-                <h2> 基本 </h2>
+                <h2> {{ $lang.basic }} </h2>
             </template>
             <div class="cc-patient-new-form">
-                <cui-input label="名前"></cui-input>
+                <cui-input required :label="$lang.name"></cui-input>
+                <cui-datepicker required :label="$lang.birthdate"></cui-datepicker>
+                <cui-input required :label="$lang.gender"></cui-input>
+                <cui-select :label="$lang.occupation"></cui-select>
+          
             </div>
         </cui-card>
-        <cui-card></cui-card>
-                <cui-card></cui-card>
-        <cui-card></cui-card>
-        <cui-card></cui-card>
+        <cui-card>
+            <template #header>
+                <h2> {{ $lang.contactInfo }} </h2>
+            </template>
+
+            <cui-input :label="$lang.zipCode"></cui-input>
+            <cui-input :label="$lang.address"></cui-input>
+            <cui-input :label="$lang.telephone"></cui-input>
+            <cui-input :label="$lang.mailAddress"></cui-input>
+
+        </cui-card>
+        <cui-card>
+            <template #header>
+                <h2> {{ $lang.workOrSchool }} </h2>
+            </template>
+            <cui-input :label="$lang.workOrSchoolName"></cui-input>
+            <cui-input :label="$lang.zipCode"></cui-input>
+            <cui-input :label="$lang.address"></cui-input>
+            <cui-input :label="$lang.telephone"></cui-input>
+
+        </cui-card>
         <cui-card></cui-card>
 
     </div>
@@ -27,7 +55,11 @@ export default {
     .cc-patient-new-main {
         height: 100%;
         display: grid;
-        grid-template-rows: 50% 50%;
-        grid-template-columns: 30% 30% 40%;
+        grid-template-rows: 85px 400px;
+        grid-template-columns: 20% 20% 20% 40%;
+        grid-template-areas: 'header header header header';
+    }
+    .cc-patient-new-header {
+        grid-area: header;
     }
 </style>
