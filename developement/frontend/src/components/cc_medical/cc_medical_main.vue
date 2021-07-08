dfaabf090db876a4730b8c3701144aa1c13802b5
jujussel <jujussel@gmail.com>
2021-07-08 20:39:12 +0900

    sync

--- developement/frontend/src/components/cc_medical/cc_medical_main.vue
+++ developement/frontend/src/components/cc_medical/cc_medical_main.vue
@@ -1,102 +1,69 @@
 <template>
     <div class="cc-medical-main-cont">
-        <cui-card noPadding>
-            <cui-table :data="encounters" :loading="loading.encounters">
-                    <template #emptyImage>
-                    <img src="../../assets/img/empty2.jpg" style="width: 300px">
-                </template>
-                <template #header>
-                    <div style="display: flex; align-items: center">
-                        <h2>{{ $lang.reception }} {{ $lang.list }} </h2>
-                    </div>
-                    <div style="display: flex; align-items: center">
-                        <cui-checkbox v-model="viewEncounters.waiting" :label="$lang.waiting" />
-                        <cui-checkbox v-model="viewEncounters.examination" style="margin-left: 20px" :label="$lang.inEncounter" />
-                        <cui-checkbox v-model="viewEncounters.done" style="margin-left: 20px; margin-right: 40px" :label="$lang.paymentDone" />
+        <cui-button-group v-model="activeTab">
+            <cui-button-group-item icon="fas fa-sign-out-alt" :label="$lang.patientList" value="encounterList"></cui-button-group-item>
+            <cui-button-group-item 
+                v-for="(item, index) in dynamicTabs" :key="index" 
+                :label="item.label"
+                :value="item.value"
+                :icon="item.icon"
+                :index="index"
+            />
 
-                    </div>
-                </template>
-                <template #thead>
-                    <cui-th sort="name">{{ $lang.name }}</cui-th>
-                    <cui-th sort="examType">{{ $lang.encounterType }}</cui-th>
-                    <cui-th sort="status">{{ $lang.status }}</cui-th>
-                    <cui-th sort="waitTime">{{ $lang.waitTime }}</cui-th>
-                </template>
-                <template v-slot:row="{ row }">
-                    <td> {{ row.patient?.name }} </td>
-                    <td> 
-                        <cui-tag primary>
-                            {{ parseExamType(row.type) }}
-                        </cui-tag>
-                    </td>
-                    <td>
-                        {{ encounterStati[row.status] }}
-                    </td>
-                    <td> 
-                        <span v-if="row.status !== 1">{{ parseWaitTime(row.lastChange).time }}</span> 
-                    </td>
-                </template>
-            </cui-table>
-        </cui-card>
+        </cui-button-group>
+        <div class="cc-medical-main-tab-hidden" v-bind:class="{ 'cc-medical-main-tab-visible': 'encounterList' === activeTab }">
+            <encounterList 
+                :key="listResetter"
+                @showEncounter="showEncounter"
+            />
+        </div>
+        <div v-for="(tab, index) in dynamicTabs" :key="index" class="cc-medical-main-tab-hidden" v-bind:class="{ 'cc-medical-main-tab-visible': tab.value === activeTab }">
+            <component
+             :is="tabContent(tab.value)"
+             @cancel="closeTab"
+            ></component>
+        </div>
     </div>
 </template>
 
 <script>
+import encounterList from "./cc_medical_list.vue";
+
 export default {
-        created() {
-        this.getEncounters();
-        this.getEncounterTypes();
+    components: {
+        encounterList
     },
     data() {
         return {
-            loading: {
-                encounters: false
-            },
-            encounters: [],
-            viewEncounters: {
-                waiting: true,
-                examination: false,
-                done: false,
-            },
-            encounterStati: {
-                0: '会計済み',
-                1: '予約',
-                2: '待ち',
-                3: '診察中',
-                4: '健康診断中',
-                5: 'オーダー待ち',
-                6: 'オーダー待ち',
-                10: '会計待ち',
-                35: '再開待ち',
-                45: '健康診断中・医者'
-            }
+            listResetter: 1,
+            activeTab: 'encounterList',
+            dynamicTabs: []
         }
     },
     methods: {
-        async getEncounterTypes() {
-            return await this.$dataService().get.lists.encounterTypes()
+        tabContent(tab) {
+            return tab.split('_')[0];
         },
-        async getEncounters() {
-            this.loading.encounters = true
-            this.encounters = await this.$dataService().get.encounters.today()
-            this.loading.encounters = false
+        showEncounter(enc) {
+            let key = this.dynamicTabs.length;
+            let item = {label: this.$lang.patientNew, value: 'encounterEdit_' + key, icon: 'fas fa-user-plus',  index: key, meta: enc.row};
+            this.dynamicTabs.push(item);
+            this.$nextTick(() => {
+                this.activeTab = item.value;
+            })
         },
-        parseExamType(type) {
-            const types = this.$store.getters.encounterTypes
-            let string = ''
-            string = types?.find(item => item.id == type).name
-            return string
+        closeTab(index = null) {
+            if (!index) {
+                let value = this.activeTab;
+                index = this.dynamicTabs.indexOf(value);
+            }
+            this.dynamicTabs.splice(index, 1);
+            if (this.dynamicTabs.length < 1) {
+                this.activeTab = 'encounterList';
+            } else {
+                this.activeTab = this.dynamicTabs[index - 1];
+            }
         },
-        parseWaitTime(change) {
-            let time = this.$moment(change).fromNow(true);
-            let diff = this.$moment().diff(this.$moment(change), "minutes");
-            return {
-                time: time,
-                diff: diff,
-            };
-        },
-
-
     }
 
 }
@@ -105,7 +72,15 @@ export default {
 <style scoped>
     .cc-medical-main-cont {
         display: grid;
-        grid-template-columns: 50% 50%;
-        grid-template-rows: 100%;
+        grid-template-columns: 100%;
+        grid-template-rows: 5% 95%;
+        height: 100%
     }
+    .cc-medical-main-tab-hidden {
+        display: none;
+    }
+    .cc-medical-main-tab-visible {
+        display: block;
+    }
+
 </style>
