ab524338d8ec32c2230e7c9db887d9d8d70d379b
Peter Jussel <jujussel@gmail.com>
2021-09-08 11:23:41 +0900

    sync

--- developement/frontend/src/config/global.js
+++ developement/frontend/src/config/global.js
@@ -1,5 +1,5 @@
 export default {
-    websocketUrl: "wss://" + window.location.hostname + ":3003", // Notification Server
+    websocketUrl: "wss://" + window.location.hostname + ':3003', // Notification Server
     schemaUrl: "/assets/schemas/",
     assetsUrl: "/assets/", //Images
     filesUrl: "/files/", //User created assets
