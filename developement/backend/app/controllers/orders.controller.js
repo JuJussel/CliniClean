44f7cf69f31ecc84d86678ba3e6f6b674ad71efd
JuJussel <jujussel@gmail.com>
2021-10-29 06:55:25 +0900

    sync

--- developement/backend/app/controllers/orders.controller.js
+++ developement/backend/app/controllers/orders.controller.js
@@ -39,65 +39,70 @@ exports.update = (req,res) => {
 
   const Encounter = require("../models/encounter.model.js");
   const Notification = require("../models/notification.model.js");
-  Order.findOneAndUpdate(
-    { _id: req.params.orderId }, 
-    req.body,
-    { runValidators: true }
+
+  try {
+    await Order.findOneAndUpdate(
+      { _id: req.params.orderId }, 
+      req.body,
+      { runValidators: true }
+    );
+  } catch (err) {
+    $logger.error(err)
+    res.status(500).send({
+      message: "Error updating Order"
+    });
+  }
+
+
+  try {
+    await Encounter.findOneAndUpdate(
+      {_id: req.body.encounterId, "karte.procedures.order.id": req.params.orderId},
+      {
+        "karte.procedures.$.order.locked": req.body.locked,
+        "karte.procedures.$.order.done": req.body.status === 0 ? true : false,
+        "karte.procedures.$.varData": req.body.procedure.var || null,
+        "karte.procedures.$.comment": req.body.procedure.comment || ''
+      }
     )
-    .then(() => {
-      Encounter.findOneAndUpdate(
-        {_id: req.body.encounterId, "karte.procedures.order.id": req.params.orderId},
-        {
-          "karte.procedures.$.order.locked": req.body.locked,
-          "karte.procedures.$.order.done": req.body.status === 0 ? true : false,
-          "karte.procedures.$.varData": req.body.procedure.var || null,
-          "karte.procedures.$.comment": req.body.procedure.comment || ''
+  } catch (err) {
+    $logger.error(err)
+    res.status(500).send({
+      message: "Error updating Encounter"
+    });
+  } 
+
+
+  try {
+    await Notification.create(
+      {
+        sender: req.userId,
+        recepients: [req.body.requester.id],
+        content: {
+          meta: req.body.id,
+          text: ""
         }
-      )
-      .then(() => {
-        let text = 
-        Notification.create(
-          {
-            sender: req.userId,
-            recepients: [req.body.requester.id],
-            content: {
-              meta: req.body.id,
-              text: ""
-            }
-          }
-        )
-        .then(() => {
-          $wss.broadcast({
-            event: "updateOrder",
-            data: {
-              encounterId: req.body.encounterId,
-              id: req.body.id,
-              locked: req.body.locked,
-              done: req.body.status === 0 ? true : false
-            },
-          });
-          res.send({ok: true});
-        })
-        .catch(err => {
-          $logger.error(err)
-          res.status(500).send({
-            message: "Error creating Notifications"
-          });
-        })      
-      })
-      .catch(err => {
-        $logger.error(err)
-        res.status(500).send({
-          message: "Error updating Order"
-        });
-      })    
-    })
-    .catch(err => {
-      $logger.error(err)
-      res.status(500).send({
-        message: "Error updating Order"
-      });
-    })
+      }
+    )
+  } catch (err) {
+    $logger.error(err)
+    res.status(500).send({
+      message: "Error creating Notifications"
+    });
+  } 
+
+
+  $wss.broadcast({
+    event: "updateOrder",
+    data: {
+      encounterId: req.body.encounterId,
+      id: req.body.id,
+      locked: req.body.locked,
+      done: req.body.status === 0 ? true : false
+    },
+  });
+
+  
+  res.send({ok: true});
 }
 
 exports.delete = (req,res) => {
