6f3d7193ccef088a1dd1c55fb3d885caeb560ebf
jujussel <jujussel@gmail.com>
2021-11-01 07:45:59 +0900

    sync

--- developement/frontend/src/components/shared/cc_shared_notifications.vue
+++ developement/frontend/src/components/shared/cc_shared_notifications.vue
@@ -4,18 +4,26 @@
             <img src="/img/empty2.8ccd6968.jpg" style="width: 200px;">
             <b> {{ $lang.noNotifications }} </b>
         </div>
+        <div v-for="(item, index) in notifications" :key="index" class="notification-item">
 
-        <cui-table v-else :data="notifications" square style="margin: 5px -10px">
-            <template v-slot:row="{ row }">
-                <td class="not-cell">
-                    <div v-if="row.content.meta.type === 'examResultsAvailable'">
-                        <i class="fas fa-microscope" style="margin-right: 10px; font-size: 20px" />
-                        <span> {{ $lang.examResultsAvailable }} </span>
+            <div v-if="item.content.meta.type === 'examResultsAvailable'" style="display:flex">
+                <div style="width:50px; height:50px; display:flex; align-items:center; justify-content:center">
+                    <i class="fas fa-microscope" style="margin-right: 10px; font-size: 40px" />
+                </div>
+                <div>
+                    <div>
+                        <span style="font-weight: normal; color: var(--cui-primary)"> {{ $lang.examResultsAvailable }} </span>
                     </div>
-                </td>
-            </template>
+                    <div>
+                        <span> {{ item.content.meta.order.patient.name }} </span>
+                        <span> {{ item.content.meta.order.procedure.name }} </span>
+                    </div>
+                </div>
+            </div>
 
-        </cui-table>
+        </div>
+
+
     </div>
 </template>
 
@@ -35,8 +43,13 @@ export default {
         flex-direction: column;
         align-items: center;
     }
-    .not-cell {
-        min-width: 100px;
-        padding: 30px;
+    .notification-item {
+        padding: 15px;
+        border-radius: 10px;
+        transition: all .2s ease;
+    }
+    .notification-item:hover {
+        background: var(--cui-gray-2);
+        cursor: pointer;
     }
      </style>
