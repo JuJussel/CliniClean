661e0ce4b0baa34b54672bb5628ba64e1b9a24bb
Peter Jussel <jujussel@gmail.com>
2021-11-16 16:28:53 +0900

    sync

--- developement/frontend/src/components/shared/cc_shared_healt_check.vue
+++ developement/frontend/src/components/shared/cc_shared_healt_check.vue
@@ -1,9 +1,83 @@
 <template>
     <div>
-        <div v-for="(item, index) in input" :key="index">
-            <cui-switch v-if="item.type === 'boolean'" v-model="item.value"></cui-switch>
-            <cui-input v-if="item.type === 'text'"></cui-input>
+        <h3> {{ $lang.historyFull }} </h3>
+        <div class="switch-cont">
+            <cui-switch :label="$lang.medicationHistory" v-model="input.medicationHistory.value"/>
+            <cui-textarea
+                v-if="input.medicationHistory.value" 
+                v-model="input.medicationHistory.note"
+                noNote 
+                :placeholder="$lang.input" 
+                style="margin-left: 10px"/>
         </div>
+        <div class="switch-cont">
+            <cui-switch :label="$lang.medicalHistory" v-model="input.medicalHistory.value"/>
+            <cui-textarea
+                v-if="input.medicalHistory.value" 
+                v-model="input.medicalHistory.note"
+                noNote 
+                :placeholder="$lang.input" 
+                style="margin-left: 10px"/>
+        </div>
+        <div class="switch-cont">
+            <cui-switch :label="$lang.subjectiveSymtoms" v-model="input.subjectiveSymtoms.value"/>
+            <cui-textarea
+                v-if="input.subjectiveSymtoms.value" 
+                v-model="input.subjectiveSymtoms.note"
+                noNote 
+                :placeholder="$lang.input" 
+                style="margin-left: 10px"/>
+        </div>
+        <div class="switch-cont">
+            <cui-switch :label="$lang.objectiveSymtoms" v-model="input.objectiveSymtoms.value"/>
+            <cui-textarea
+                v-if="input.objectiveSymtoms.value" 
+                v-model="input.objectiveSymtoms.note"
+                noNote 
+                :placeholder="$lang.input" 
+                style="margin-left: 10px"/>
+        </div>
+
+        <h3> {{ $lang.basic }} </h3>
+        <div style="display: flex">
+            <cui-input :label="$lang.height" v-model="input.height.value" type="number" style="max-width: 150px" />
+            <cui-input :label="$lang.weight" v-model="input.weight.value" type="number" style="max-width: 150px; margin-left: 20px" />
+        </div>
+        <div style="display: flex">
+            <cui-input :label="$lang.bmi" v-model="input.bmi.value" type="number" style="max-width: 150px" />
+            <cui-input :label="$lang.stomacheWidth" v-model="input.stomacheWidth.value" type="number" style="max-width: 150px; margin-left: 20px" />
+        </div>
+
+        <h3> {{ $lang.bloodPreasure }} </h3>
+        <div style="display: flex">
+            <cui-input :label="$lang.bloodPreasureMin" v-model="input.bloodPreasureMin.value" type="number" style="max-width: 150px" />
+            <cui-input :label="$lang.bloodPreasureMax" v-model="input.bloodPreasureMax.value" type="number" style="max-width: 150px; margin-left: 20px" />
+        </div>
+
+        <h3> {{ $lang.eyeSight }} </h3>
+        <div style="display: flex">
+            <cui-input :label="$lang.sightLeft" v-model="input.sightLeft.value" type="number" style="max-width: 150px" />
+            <cui-input :label="$lang.sightRight" v-model="input.sightRight.value" type="number" style="max-width: 150px; margin-left: 20px" />
+        </div>
+
+        <h3> {{ $lang.hearingEvaluation }} </h3>
+        <div style="display: flex">
+            <cui-input :label="$lang.hearingLeftLow" v-model="input.hearingLeftLow.value" type="number" style="max-width: 150px" />
+            <cui-input :label="$lang.hearingLeftHight" v-model="input.hearingLeftHight.value" type="number" style="max-width: 150px; margin-left: 20px" />
+        </div>
+        <div style="display: flex">
+            <cui-input :label="$lang.hearingRightLow" v-model="input.hearingRightLow.value" type="number" style="max-width: 150px" />
+            <cui-input :label="$lang.hearingRightHigh" v-model="input.hearingRightHigh.value" type="number" style="max-width: 150px; margin-left: 20px" />
+        </div>
+
+        <h3> {{ $lang.other }} </h3>
+
+
+        <!-- <div v-for="(item, index) in input" :key="index">
+            <cui-input :label="$lang[item.label]" v-if="item.type === 'text'" style="max-width: 150px"></cui-input>
+            <cui-input :label="$lang[item.label]" v-if="item.type === 'number'" type="number" style="max-width: 150px"></cui-input>
+
+        </div> -->
     </div>
 </template>
 
@@ -11,21 +85,35 @@
 export default {
     data() {
         return {
-            input: [
-                {label: "medicationHistory", value: false, type: "boolean", error: false},
-                {label: "MedicationHistoryText", value: null, type: "text", error: false},
-                {label: "medicalHistory", value: false, type: "boolean", error: false},
-                {label: "medicalHistoryText", value: null, type: "text", error: false},
-                {label: "subjectiveSymtoms", value: false, type: "boolean", error: false},
-                {label: "subjectiveSymtomsText", value: null, type: "text", error: false},
-                {label: "objectiveSymtoms", value: false, type: "boolean", error: false},
-                {label: "objectiveSymtomsText", value: null, type: "text", error: false},
+            input: {
+                medicationHistory: {value: false, note: ""},
+                medicalHistory: {value: false, note: ""},
+                subjectiveSymtoms: {value: false, note: ""},
+                objectiveSymtoms: {value: false, note: ""},
+                height: {value: ""},
+                weight: {value: ""},
+                bmi: {value: ""},
+                stomacheWidth: {value: ""},
+                bloodPreasureMax: {value: ""},
+                bloodPreasureMin: {value: ""},
+                sightLeft: {value: ""},
+                sightRight: {value: ""},
+                hearingLeftLow: {value: ""},
+                hearingLeftHight: {value: ""},
+                hearingRightLow: {value: ""},
+                hearingRightHigh: {value: ""},
+                ecgId: {value: ""},
+                xRayId: {value: ""},
+            },
+            input2: [
+                {label: "medicationHistory", value: true, type: "boolean", error: false, note: ""},
+                {label: "medicalHistory", value: false, type: "boolean", error: false, note: ""},
+                {label: "subjectiveSymtoms", value: false, type: "boolean", error: false, note: ""},
+                {label: "objectiveSymtoms", value: false, type: "boolean", error: false, note: ""},
                 {label: "height", value: null, type: "number", error: false},
                 {label: "weight", value: null, type: "number", error: false},
                 {label: "bmi", value: null, type: "number", error: false},
                 {label: "stomacheWidth", value: null, type: "number", error: false},
-                {label: "ecg", value: null, type: "text", error: false},
-                {label: "xRayId", value: null, type: "text", error: false},
                 {label: "bloodPreasureMax", value: null, type: "number", error: false},
                 {label: "bloodPreasureMin", value: null, type: "number", error: false},
                 {label: "sightLeft", value: null, type: "number", error: false},
@@ -34,9 +122,20 @@ export default {
                 {label: "hearingLeftHight", value: false, type: "boolean", error: false},
                 {label: "hearingRightLow", value: false, type: "boolean", error: false},
                 {label: "hearingRightHigh", value: false, type: "boolean", error: false},
-                {label: "xRaySchemaId", value: false, type: "boolean", error: false}
-            ]
+                {label: "ecgId", value: null, type: "text", error: false},
+                {label: "xRayId", value: null, type: "text", error: false},
+            ],
+            xRaySchemaId: null
         }
     }
 }
 </script>
+
+<style scoped>
+    .switch-cont {
+        margin: 10px 0;
+        display: flex;
+        overflow: auto;
+    }
+    
+</style>
