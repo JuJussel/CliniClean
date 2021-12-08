ee90c3c2d985c4a541b45dc240a3fcfd9b38fbfc
JuJussel <jujussel@gmail.com>
2021-12-09 07:40:54 +0900

    sync

---
+++ developement/frontend/src/components/shared/cc_shared_procedures_list/cc_shared_procedure_health_check.vue
@@ -0,0 +1,61 @@
+<template>
+    <div style="display: flex">
+        <cui-select
+            :data="shotLocations"
+            v-model="varData.location"
+            style="width: 100px"
+            :label="$lang.shotLocation"
+            @update:modelValue="updateValue"
+            :disabled="item.order?.done || item.order?.locked"
+        />
+        <cui-input
+            style="width: 100px; margin-left: 30px"
+            v-model="varData.amount"
+            :append="$lang.vial"
+            :label="$lang.shotAmount"
+            @update:modelValue="updateValue"
+            :disabled="item.order?.done || item.order?.locked"
+        />
+        <cui-input
+            style="width: 100px; margin-left: 30px"
+            v-model="varData.lot"
+            :label="$lang.shotLot"
+            @update:modelValue="updateValue"
+            :disabled="item.order?.done || item.order?.locked"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            default: null,
+        },
+    },
+    created() {
+        this.setData();
+    },
+    emits: ["update"],
+    methods: {
+        updateValue() {
+            this.$emit("update", this.varData);
+        },
+        setData() {
+            if (this.item.varData) {
+                this.varData = this.item.varData;
+            }
+        },
+    },
+    data() {
+        return {
+            shotLocations: this.$store.getters.staticLists.shotLocations,
+            varData: {
+                location: null,
+                amount: "",
+                lot: "",
+            },
+        };
+    },
+};
+</script>
