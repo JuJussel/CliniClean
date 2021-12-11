8316f0d2e9259b3f52765b88aa3a78aff83a64db
JuJussel <jujussel@gmail.com>
2021-12-12 08:19:52 +0900

    sync

--- developement/frontend/src/components/shared/cc_shared_health_check/cc_shared_health_check.vue
+++ developement/frontend/src/components/shared/cc_shared_health_check/cc_shared_health_check.vue
@@ -47,11 +47,11 @@
             </div>
             <div style="display: flex">
                 <cui-input @update:modelValue="calcBMI" :label="$lang.height" v-model="input.height.value" type="number" style="max-width: 150px" :append="input.height.unit"/>
-                <cui-input @update:modelValue="calcBMI" :label="$lang.weight" v-model="input.weight.value" type="number" style="max-width: 150px; margin-left: 20px"  :append="input.weight.unit"/>
+                <cui-input @update:modelValue="calcBMI" :label="$lang.weight" v-model="input.weight.value" type="number" style="max-width: 150px; margin-left: 20px" :append="input.weight.unit"/>
             </div>
             <div style="display: flex">
                 <cui-input :label="$lang.bmi" v-model="input.bmi.value" type="number" style="max-width: 150px" />
-                <cui-input :label="$lang.stomacheWidth" v-model="input.stomacheWidth.value" type="number" style="max-width: 150px; margin-left: 20px"  :append="input.stomacheWidth.unit"/>
+                <cui-input :label="$lang.stomacheWidth" v-model="input.stomacheWidth.value" type="number" style="max-width: 150px; margin-left: 20px" :append="input.stomacheWidth.unit"/>
             </div>
             <div style="display: flex">
                 <cui-input :label="$lang.bloodPreasureMin" v-model="input.bloodPreasureMin.value" type="number" style="max-width: 150px" :append="input.bloodPreasureMin.unit"/>
@@ -144,12 +144,12 @@ export default {
                 medicalHistory: {value: false, note: ""},
                 subjectiveSymtoms: {value: false, note: ""},
                 objectiveSymtoms: {value: false, note: ""},
-                height: {value: "", unit: "cm", vitalCode: this.$store.getters.staticLists.vitalCategories.find(i => i.code === 909)},
-                weight: {value: "", unit: "kg", vitalCode: 906},
-                bmi: {value: "", vitalCode: 907},
-                stomacheWidth: {value: "", unit: "cm", vitalCode: 908},
-                bloodPreasureMax: {value: "", unit: "mmHg", vitalCode: 901},
-                bloodPreasureMin: {value: "", unit: "mmHg", vitalCode: 902},
+                height: Object.assign( this.$store.getters.staticLists.vitalCategories.find(i => i.code === 909), {value: ""}),
+                weight: Object.assign( this.$store.getters.staticLists.vitalCategories.find(i => i.code === 906), {value: ""}),
+                bmi: Object.assign( this.$store.getters.staticLists.vitalCategories.find(i => i.code === 907), {value: ""}),
+                stomacheWidth: Object.assign( this.$store.getters.staticLists.vitalCategories.find(i => i.code === 908), {value: ""}),
+                bloodPreasureMax: Object.assign( this.$store.getters.staticLists.vitalCategories.find(i => i.code === 901), {value: ""}),
+                bloodPreasureMin: Object.assign( this.$store.getters.staticLists.vitalCategories.find(i => i.code === 902), {value: ""}),
                 sightLeft: {value: ""},
                 sightRight: {value: ""},
                 hearingLeftLow: {value: ""},
