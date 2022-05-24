e1ba9ca591f58d197e7badf4c07de3b6352bf345
JuJussel <jujussel@gmail.com>
2022-05-24 17:13:51 +0900

    sync

--- developement/frontend/src/components/shared/cc_shared_chart/cc_shared_chart_main.vue
+++ developement/frontend/src/components/shared/cc_shared_chart/cc_shared_chart_main.vue
@@ -7,10 +7,51 @@
 <script>
 export default {
     props: {
-        chartData: {
+        dataSet: {
             type: Object,
             default: null
+        },
+        options: {
+            type: Object,
+            default: null
+        },
+        type: {
+            type: String,
+            default: "line"
+        }
+    },
+    data() {
+        return {
+            defaultOptions: {
+                line: {
+                    xAxis: {
+                        type: "category",
+                        data: []
+                    },
+                    yAxis: {
+                        type: "value"
+                    },
+                    series: [
+                        {
+                            data: [],
+                            type: "line"
+                        }
+                    ]
+                }
+            }
+        };
+    },
+    computed: {
+        computedOptions() {
+            if (this.options) {
+                return this.options;
+            } else {
+                let options = this.defaultOptions[this.type];
+                options.xAxis.data = this.dataSet.xAxis;
+                options.series.data = this.dataSet.values;
+                return options;
+            }
         }
     }
-}
+};
 </script>
--- developement/frontend/src/components/shared/cc_shared_patient_info_medical/cc_shared_patient_medical_chart.vue
+++ developement/frontend/src/components/shared/cc_shared_patient_info_medical/cc_shared_patient_medical_chart.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-        <chart :chartData="localData.charts[0].data" />
+        <chart
+            :type="localData.charts[1].options.type"
+            :dataSet="localData.charts[1].dataSet"
+        />
     </div>
 </template>
 
 <script>
-
-import chart from '../cc_shared_chart/cc_shared_chart_main.vue'
+import chart from "../cc_shared_chart/cc_shared_chart_main.vue";
 
 export default {
     components: {
-        chart,
+        chart
     },
     data() {
         return {
@@ -28,67 +30,68 @@ export default {
                 },
                 charts: [
                     {
-                        name: 'timeline', 
-                        type: 'gantt', 
-                        selected: {
-                            encounters: true,
-                            vitals: true,
-                            diseases: true,
-                            exams: true,
-                            medications: true,
-                            operations: true,
-                            shots: true,
-                            prevVacs: true,
-                            treatments: true
-                        }, 
-                        data: {
-                            id: 'events',
-                            type: 'custom',
-                            dimensions: [
-                                'type',
-                                'startDate',
-                                'endDate',
-                                'id'
-                            ],
+                        options: {
+                            name: "timeline",
+                            type: "gantt",
+                            selected: {
+                                encounters: true,
+                                vitals: true,
+                                diseases: true,
+                                exams: true,
+                                medications: true,
+                                operations: true,
+                                shots: true,
+                                prevVacs: true,
+                                treatments: true
+                            }
+                        },
+                        dataSet: {
+                            id: "events",
+                            type: "custom",
+                            dimensions: ["type", "startDate", "endDate", "id"],
                             encode: {
-                                x: [1,2],
+                                x: [1, 2],
                                 y: 0,
-                                tooltip: [0,1,3]
+                                tooltip: [0, 1, 3]
                             },
                             data: []
-                        }},
-                    {name: 'vitals', type: 'line'}
+                        }
+                    },
+                    {
+                        options: {
+                            name: "vitals",
+                            type: "line"
+                        },
+                        dataSet: []
+                    }
                 ]
             },
-            workingViewData: null,
-        }
+            workingViewData: null
+        };
     },
     created() {
-        this.workingViewData = JSON.parse(JSON.stringify(this.$store.getters.viewData));
+        this.workingViewData = JSON.parse(
+            JSON.stringify(this.$store.getters.viewData)
+        );
         this.transformPatientData();
     },
     methods: {
         commitworkingViewData() {
-            this.$store.commit('SET_VIEW_DATA', this.workingViewData)
+            this.$store.commit("SET_VIEW_DATA", this.workingViewData);
         },
-		transformPatientData() {
-			let patientData = JSON.parse(JSON.stringify(this.$store.getters.activePatientHistory));
-			// Encounters
-			patientData.encounters.forEach(enc => {
-                if (this.localData.charts[0].selected.encounters) {
-                    let arr = [
-                        enc.type,
-                        enc.date,
-                        enc.lastChange,
-                        enc.id
-                    ];
-                    this.localData.charts[0].data.data.push(arr);
+        transformPatientData() {
+            let patientData = JSON.parse(
+                JSON.stringify(this.$store.getters.activePatientHistory)
+            );
+            // Encounters
+            patientData.encounters.forEach(enc => {
+                if (this.localData.charts[0].options.selected.encounters) {
+                    let arr = [enc.type, enc.date, enc.lastChange, enc.id];
+                    this.localData.charts[0].dataSet.data.push(arr);
                 }
-			})
-		}
+            });
+        }
     },
-    computed: {
-	}
-
-}
+    computed: {}
+};
 </script>
