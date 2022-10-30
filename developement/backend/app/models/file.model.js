97a85c8e2d8923603cccd759cf2b6f3e1debaa7f
JuJussel <jujussel@gmail.com>
2022-10-30 18:10:16 +0900

    sync

--- developement/backend/app/models/file.model.js
+++ developement/backend/app/models/file.model.js
@@ -1,11 +1,15 @@
-const mongoose = require('./db.mongo')
+const mongoose = require("./db.mongo");
 const Schema = mongoose.Schema;
 
-const FileSchema = new Schema({
-    extension: {type:String, required: true},
-    size: Number,
-    type: {type:String, required: true},
-    meta: {}
-},{ toJSON: { virtuals: true } });
+const FileSchema = new Schema(
+    {
+        extension: { type: String, required: true },
+        size: Number,
+        type: { type: String, required: true },
+        meta: {},
+        date: { type: Date, default: Date.now() },
+    },
+    { toJSON: { virtuals: true } }
+);
 
 module.exports = mongoose.model("pub_files", FileSchema);
--- developement/frontend_vite/src/components/parts/cc_p_patient_info_medical/cc_p_patient_info_medical_files.vue
+++ developement/frontend_vite/src/components/parts/cc_p_patient_info_medical/cc_p_patient_info_medical_files.vue
@@ -1,6 +1,8 @@
 <template>
     <cui-card noPadding style="max-height: 100%">
-        <cui-tree></cui-tree>
+        <cui-tree v-if="treeData.length > 0" :nodes="treeData">
+            <template #title>Files</template>
+        </cui-tree>
     </cui-card>
 </template>
 
@@ -20,6 +22,39 @@ export default {
                 this.$store.getters.layoutData.medical.patient.files || [];
             let files = await this.$dataService().get.files(fileList);
             this.files = files;
+        },
+    },
+    computed: {
+        treeData() {
+            let tree = [];
+            this.files.forEach((item) => {
+                let type = item.type.split("/")[0];
+                let index = tree.findIndex((i) => i.name === type);
+                if (index < 0) {
+                    tree.push({ name: type });
+                    index = 0
+                }
+                if (type === "image") {
+                    let source = item.meta.source
+                    let name = item.meta.symbol ? item.meta.symbol + "-" + item.meta.number : item.date || item.id
+                    if (tree[index].children) {
+                        let index2 = tree[index].children.findIndex((i) => i.name === source);
+                        console.log(index2);
+                        if (index2 < 0) {
+                            tree[index]?.children.push({ name: source, children: [] });
+                            index2 = tree[index].children.length - 1
+                        }
+                        console.log(JSON.stringify(tree[index].children[index2]));
+                        tree[index].children[index2].children.push({ name: name})
+                    } else {
+                        tree[index].children = [{ name: source, children: [{ name: name }] }]                        
+                    }
+                }
+
+
+            });
+
+            return tree;
         },
     },
 };
