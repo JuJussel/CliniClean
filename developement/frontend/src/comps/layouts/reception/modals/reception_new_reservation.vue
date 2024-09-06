d2854ade358de5f64963817933ea7f228ccbcf21
jujussel <jujussel@gmail.com>
2024-09-06 21:13:24 +0900

    sync

--- developement/frontend/src/comps/layouts/reception/modals/reception_new_reservation.vue
+++ developement/frontend/src/comps/layouts/reception/modals/reception_new_reservation.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="grid grid-cols-2 gap-16">
-        <div></div>
+        <div>
+            <div class="flex flex-col gap-1 mb-4">
+                <label>{{ $t('encounterType') }}</label>
+                <Select v-model="encounterType" :options="listStore.listData.encounterTypes" optionLabel="name" placeholder="Select a City" class="w-full md:w-56" />
+            </div>
+        </div>
         <div style="height: 500px">
-            <Calendar ref="calendar" :reRender="350" @selectDate="" />
-
+            <Calendar ref="calendar" :reRender="350" @selectDate="selectDate" />
         </div>
     </div>
     <div class="flex justify-end gap-2">
@@ -14,10 +18,25 @@
 </template>
 
 <script setup>
-
+import Select from 'primevue/select';
 import Calendar from '../../../shared/calendar.vue'
+import dayjs from "dayjs"
+import useApi from "@/composables/apiComposable.js"
+
+const listStore = useListStore()
 
 const props = defineProps(['patient'])
 const emit = defineEmits(['close', 'commit'])
+const date = ref(null)
+const time = ref(null)
+const encounterType = ref(listStore.listData.encounterTypes[0])
+const note = ref('')
 
+const selectDate = (i) => {
+    console.log(i);
+    date.value = dayjs(i.start).format("YYYY-MM-DD");
+    if (!date.allDay) {
+        time.value = dayjs(i.start).format("HH:mm:ss");
+    }
+}
 </script>
--- developement/frontend/src/comps/shared/calendar.vue
+++ developement/frontend/src/comps/shared/calendar.vue
@@ -1,33 +1,5 @@
 <template>
     <div class="h-full p-2">
-        <!-- <cui-button-group @change="changeView">
-                <cui-button-group-item
-                    :label="$lang.prev"
-                    :value="1"
-                ></cui-button-group-item>
-                <cui-button-group-item
-                    :label="$lang.today"
-                    :value="2"
-                ></cui-button-group-item>
-                <cui-button-group-item
-                    :label="$lang.next"
-                    :value="3"
-                ></cui-button-group-item>
-            </cui-button-group>
-            <cui-button-group @change="changeView">
-                <cui-button-group-item
-                    :label="$lang.day"
-                    value="timeGridDay"
-                ></cui-button-group-item>
-                <cui-button-group-item
-                    :label="$lang.week"
-                    value="timeGridWeek"
-                ></cui-button-group-item>
-                <cui-button-group-item
-                    :label="$lang.month"
-                    value="dayGridMonth"
-                ></cui-button-group-item>
-            </cui-button-group> -->
         <div class="flex gap-2 -mb-7">
             <div>
                 <InputGroup>
@@ -55,9 +27,14 @@ import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import dayjs from "dayjs"
+import useApi from "@/composables/apiComposable.js"
 
-const fullCalendar = ref(null);
-let calApi = ref(null);
+const emit = defineEmits(['selectDate'])
+
+const fullCalendar = ref(null)
+const eventsLoading = ref(false)
+let calApi = ref(null)
 
 onMounted(() => {
     calApi = fullCalendar.value.getApi();
@@ -71,13 +48,11 @@ const refetchEvents = () => {
 }
 
 const selectDate = (date) => {
-    console.log(date);
-
-    // this.$emit("selectDate", date);
+    emit('selectDate', date)
 }
 
 const eventClick = (data) => {
-    // this.$emit("eventClick", data);
+    emit('eventSelect', data)
 }
 
 const navigateView = (action) => {
@@ -91,40 +66,27 @@ const navigateView = (action) => {
 }
 
 const changeView = (view) => {
-    console.log(view);
-
     calApi.changeView(view)
-    // if (view === 1) {
-    //     this.cal.prev();
-    // } else if (view === 2) {
-    //     this.cal.today();
-    // } else if (view === 3) {
-    //     this.cal.next();
-    // } else {
-    //     this.cal.changeView(view);
-    // }
 }
+
 const getEvents = async (i, successCallback) => {
-    // let start = this.$dayjs(i.start.valueOf()).format(
-    //     "YYYY-MM-DD HH:mm:ss"
-    // );
-    // let end = this.$dayjs(i.end.valueOf()).format(
-    //     "YYYY-MM-DD HH:mm:ss"
-    // );
-    // let range = "start=" + start + "&end=" + end;
-    // let events = await this.$api.get("encounters/range?" + range);
-    // events = events
-    //     .filter((item) => item.status === 0)
-    //     .map(function (event) {
-    //         return {
-    //             id: event.id,
-    //             title: event.patient.name,
-    //             start: event.date,
-    //             end: event.shinsatu_end,
-    //             meta: event,
-    //         };
-    //     });
-    // successCallback(events);
+    
+    eventsLoading.value = true
+    const range = 'start=' + dayjs(i.startStr).$d + '&end=' + dayjs(i.endStr).$d;
+
+    let events = await useApi.get("encounters/range?" + range)
+    events = events.filter((item) => item.status === 0)
+    events = events.map(function (event) {
+            return {
+                id: event.id,
+                title: event.patient.name,
+                start: event.date,
+                end: event.shinsatu_end,
+                meta: event,
+            };
+        });        
+    eventsLoading.value = false
+    successCallback(events)
 }
 
 const calendarOptions = reactive({
@@ -162,150 +124,13 @@ const calendarOptions = reactive({
         { daysOfWeek: [6], startTime: "09:00", endTime: "12:00" },
     ],
     navLinkDayClick: "timeGridDay",
-    // eventSources: [
-    //     {
-    //         events: getEvents,
-    //         color: "rgb(44, 62, 80)",
-    //     },
-    // ],
+    eventSources: [
+        {
+            events: getEvents,
+            color: "rgb(44, 62, 80)",
+        },
+    ],
 
 })
 
-
-
-
-// export default {
-//     props: {
-//         config: {
-//             default: null,
-//         },
-//         reRender: {
-//             default: null,
-//             type: Number,
-//         },
-//     },
-//     components: {
-//         FullCalendar
-//     },
-//     mounted() {
-//         let calEl = this.$refs.calendar;
-//         let calendar = new FullCalendar(calEl, this.options);
-//         calendar.render();
-//         this.cal = calendar;
-//         if (this.reRender) {
-//             setTimeout(
-//                 function () {
-//                     this.cal.render();
-//                 }.bind(this),
-//                 this.reRender
-//             );
-//         }
-//     },
-//     created() {
-//         if (this.config) {
-//             this.options = Object.assign(this.options, this.config);
-//         }
-//     },
-//     data() {
-//         return {
-//             view: "",
-//             cal: null,
-//             options: {
-//                 headerToolbar: {
-//                     left: "title",
-//                     center: "",
-//                     right: "",
-//                 },
-//                 initialView: "dayGridMonth",
-//                 plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
-//                 firstDay: 1,
-//                 displayEventTime: false,
-//                 fixedWeekCount: false,
-//                 navLinks: true,
-//                 height: "100%",
-//                 slotMinTime: "09:00:00",
-//                 slotMaxTime: "20:00:00",
-//                 allDaySlot: false,
-//                 editable: false,
-//                 locale: "ja",
-//                 selectable: true,
-//                 select: this.selectDate,
-//                 eventClick: this.eventClick,
-//                 businessHours: [
-//                     {
-//                         daysOfWeek: [1, 2, 3, 4, 5],
-//                         startTime: "09:00",
-//                         endTime: "12:00",
-//                     },
-//                     {
-//                         daysOfWeek: [1, 2, 3, 5],
-//                         startTime: "16:00",
-//                         endTime: "20:00",
-//                     },
-//                     { daysOfWeek: [6], startTime: "09:00", endTime: "12:00" },
-//                 ],
-//                 navLinkDayClick: "timeGridDay",
-//                 eventSources: [
-//                     {
-//                         events: this.getEvents,
-//                         color: "rgb(44, 62, 80)",
-//                     },
-//                 ],
-//             },
-//         };
-//     },
-//     methods: {
-//         refetchEvents() {
-//             this.cal.refetchEvents();
-//         },
-//         selectDate(date) {
-//             this.$emit("selectDate", date);
-//         },
-//         eventClick(data) {
-//             this.$emit("eventClick", data);
-//         },
-//         changeView(view) {
-//             if (view === 1) {
-//                 this.cal.prev();
-//             } else if (view === 2) {
-//                 this.cal.today();
-//             } else if (view === 3) {
-//                 this.cal.next();
-//             } else {
-//                 this.cal.changeView(view);
-//             }
-//         },
-//         async getEvents(i, successCallback) {
-//             console.log(i);
-//             let start = this.$dayjs(i.start.valueOf()).format(
-//                 "YYYY-MM-DD HH:mm:ss"
-//             );
-//             let end = this.$dayjs(i.end.valueOf()).format(
-//                 "YYYY-MM-DD HH:mm:ss"
-//             );
-//             let range = "start=" + start + "&end=" + end;
-//             let events = await this.$api.get("encounters/range?" + range);
-//             events = events
-//                 .filter((item) => item.status === 0)
-//                 .map(function (event) {
-//                     return {
-//                         id: event.id,
-//                         title: event.patient.name,
-//                         start: event.date,
-//                         end: event.shinsatu_end,
-//                         meta: event,
-//                     };
-//                 });
-//             successCallback(events);
-//         },
-//     },
-//     watch: {
-//         view() {
-//             this.$refs.calendar.fireMethod("changeView", this.view);
-//         },
-//         height() {
-//             this.$refs.calendar.fireMethod("updateSize");
-//         },
-//     },
-// };
 </script>
