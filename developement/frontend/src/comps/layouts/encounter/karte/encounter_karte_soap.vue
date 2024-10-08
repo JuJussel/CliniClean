4e09c676b190572ca015fc3031fce2dc6328819d
jujussel <jujussel@gmail.com>
2024-10-08 17:34:38 +0900

    sync

--- developement/frontend/src/comps/layouts/encounter/karte/encounter_karte_soap.vue
+++ developement/frontend/src/comps/layouts/encounter/karte/encounter_karte_soap.vue
@@ -1,14 +1,5 @@
 <template>
     <div>
-        <Editor v-model="soapText" editorStyle="height: 320px">
-            <template v-slot:toolbar>
-                <span class="ql-formats">
-                    <button v-tooltip.bottom="'Bold'" class="ql-bold"></button>
-                    <button v-tooltip.bottom="'Italic'" class="ql-italic"></button>
-                    <button v-tooltip.bottom="'Underline'" class="ql-underline"></button>
-                </span>
-            </template>
-        </Editor>
     </div>
 </template>
 
