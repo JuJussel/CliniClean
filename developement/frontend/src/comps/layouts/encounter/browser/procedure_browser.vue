50aadcfb0fc44a324799532841defc45f9adce2c
jujussel <jujussel@gmail.com>
2024-10-11 06:53:42 +0900

    sync

--- developement/frontend/src/comps/layouts/encounter/browser/procedure_browser.vue
+++ developement/frontend/src/comps/layouts/encounter/browser/procedure_browser.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
-        <InputGroup>
-            <InputGroupAddon>
+        <SelectButton
+            class="!flex flex-wrap bg-[var(--p-togglebutton-background)] mb-2"
+            v-model="activeCategory"
+            :options="filteredProcedureCategories"
+            optionValue="code"
+            aria-labelledby="basic"
+        >
+            <template #option="slotProps">
+                {{ $t(slotProps.option.label) }}
+            </template>
+        </SelectButton>
+        <IconField class="mb-2">
+            <InputIcon>
                 <i class="pi pi-search" />
-            </InputGroupAddon>
-            <InputText v-model="search" :placeholder="$t('procedureSearch')" />
-        </InputGroup>
+            </InputIcon>
+            <InputText
+                v-model="searchInput"
+                @update:modelValue="triggerSearch"
+                :placeholder="$t('procedureSearch')"
+                class="w-full"
+            />
+        </IconField>
+
         <DataView :value="listData">
             <template #list="slotProps"> Hi </template>
         </DataView>
@@ -19,12 +36,17 @@ const userStore = useUserStore();
 const listStore = useListStore();
 const favs = ref([]);
 const activeCategory = ref(listStore.listData.procedureCategories[0].code);
-const search = ref("");
+const searchInput = ref("");
+const isSearching = ref(false);
+const listData = ref(
+    favs.value.filter((item) => item.cat.code === activeCategory)
+);
 
 async () => {
     favs.value = useApi.get("users/" + userStore.userData.id + "/favourites");
 };
 
+// Functions
 const selectProcedure = async (item) => {
     favs = await useApi.put(
         "users/" + userStore.userData.id + "/favourites",
@@ -32,15 +54,34 @@ const selectProcedure = async (item) => {
     );
 };
 
+const triggerSearch = async (input) => {
+    if (searchInput.value === "") return;
+    isSearching.value = true;
+    if (activeCategory.value == 25 || activeCategory.value == 30) {
+        listData.value = await useApi.get(
+            "medications/" + activeCategory.value + "/" + searchInput.value
+        );
+    } else {
+        listData.value = await useApi.get(
+            "procedures/" +
+                activeCategory.value +
+                "/search/" +
+                searchInput.value
+        );
+    }
+    isSearching.value = false;
+};
+
+// Computes
 const filteredProcedureCategories = computed(() => {
     return listStore.listData.procedureCategories.filter(
         (item) => item.code != 90
     );
 });
 
-const listData = computed(() => {
-    if (search.value === "") {
-        return favs.value.filter((item) => item.cat.code === activeCategory);
-    }
-});
+// const listData = computed(() => {
+//     if (search.value === "") {
+//         return favs.value.filter((item) => item.cat.code === activeCategory);
+//     }
+// });
 </script>
