b73254facbe82fa2a0e595aee9797c466683a667
JuJussel <jujussel@gmail.com>
2024-10-28 18:48:38 +0900

    sync

--- developement/frontend/src/comps/layouts/patient/patient_medical/patient_medical_risks/medical_risks.vue
+++ developement/frontend/src/comps/layouts/patient/patient_medical/patient_medical_risks/medical_risks.vue
@@ -1,9 +1,33 @@
 <template>
     <div>
-        Risks
+        <DataView :value="listStore.listData.habitCategories">
+            <template #list="slotProps">
+                <div class="flex flex-col">
+                    <div v-for="(item, index) in slotProps.items" :key="index">
+                        {{ item.name }}
+                        {{ findValue(item.name) }}
+                    </div>
+                </div>
+            </template>
+        </DataView>
     </div>
 </template>
 
 <script setup>
+const listStore = useListStore();
+const patientStore = usePatientStore();
 
+const habitData = computed(() => {
+    return [];
+});
+
+const findValue = (category) => {
+    console.log(category);
+
+    return (
+        patientStore.activePatientDataMedical.habits?.find(
+            (item) => item.name === category
+        ).value || 1
+    );
+};
 </script>
