c5b97799cf11be9401ed9b6060fa9ca95e97f3e8
jujussel <jujussel@gmail.com>
2025-10-23 18:30:37 +0900

    sync asteriks

--- developement/frontend/src/config/formkit.config.js
+++ developement/frontend/src/config/formkit.config.js
@@ -1,14 +1,40 @@
 import { ja } from '@formkit/i18n'
 import { primeInputs, primeOutputs } from '@sfxcode/formkit-primevue'
 
+const legends = ['checkbox_multi', 'radio_multi', 'repeater', 'transferlist'];
+
+function addAsteriskPlugin(node) {
+    if (['button', 'submit', 'hidden', 'group', 'list', 'meta'].includes(node.props.type)) return;
+
+    node.on('created', () => {
+        const legendOrLabel = legends.includes(`${node.props.type}${node.props.options ? '_multi' : ''}`) ? 'legend' : 'label';
+
+        if (node.props.definition.schemaMemoKey) {
+            node.props.definition.schemaMemoKey += `${node.props.options ? '_multi' : ''}_add_asterisk`;
+        };
+
+        const schemaFn = node.props.definition.schema
+        node.props.definition.schema = (sectionsSchema = {}) => {
+            sectionsSchema[legendOrLabel] = {
+                children: ['$label', {
+                    $el: 'span',
+                    if: '$state.required',
+                    attrs: {
+                        class: '$classes.asterisk',
+                    },
+                    children: ['*']
+                }]
+            }
+
+            return schemaFn(sectionsSchema)
+        }
+    })
+}
+
 
 export default {
+    plugins: [addAsteriskPlugin],
     locales: { ja },
     locale: 'ja',
     inputs: { ...primeInputs, ...primeOutputs },
-    messages: {
-        ja: {
-            name: '名前',
-        }
-    }
 }
