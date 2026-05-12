a5436fa68d83428ecf110ec7d0f99b0408e8bf33
jujussel <jujussel@gmail.com>
2026-05-13 06:37:55 +0900

    sync

--- app/components/CompProcedureBrowser.vue
+++ app/components/CompProcedureBrowser.vue
@@ -1,5 +1,29 @@
-<script setup></script>
+<script setup>
+const systemStore = useSystemStore();
+
+const filteredProcedureCategories = computed(() => {
+    return systemStore.system.ui.procedureCategories.filter(
+        (item) => item.code != 90,
+    );
+});
+</script>
 
 <template>
-    <div>CompProcedureBrowser</div>
+    <UTabs
+        :items="filteredProcedureCategories"
+        color="neutral"
+        class="h-full"
+        :ui="{
+            content: 'h-full',
+            list: 'flex-wrap',
+            indicator: 'hidden',
+            trigger:
+                'data-[state=active]:bg-inverted data-[state=active]:text-inverted rounded-md',
+        }"
+    >
+        <template #default="{ item }">
+            {{ $t(item.label) }}
+        </template>
+        <template #search> </template>
+    </UTabs>
 </template>
